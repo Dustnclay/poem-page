d0c99009013eaa3518f97ae066238c7b70ba98c6
Dustin Tiner <60906954+Dustnclay@users.noreply.github.com>
2020-08-12 22:13:47 -0500

    Update App.js
    add poem

--- src/App.js
+++ src/App.js
@@ -16,15 +16,14 @@ function App() {
     <main>
       <h1>Poem Page</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        When I heard the learn’d astronomer,
+        When the proofs, the figures, were ranged in columns before me,
+        When I was shown the charts and diagrams, to add, divide, and measure them,
+        When I sitting heard the astronomer where he lectured with much applause in the lecture-room,
+        How soon unaccountable I became tired and sick,
+        Till rising and gliding out I wander’d off by myself,
+        In the mystical moist night-air, and from time to time,
+        Look’d up in perfect silence at the stars.
       </h2>
       <p>
         <a
